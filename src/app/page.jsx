a3f6d0262ba0f624fd9ed45f90416c50cc12b3cb
Coding With Thang <codingwiththang@gmail.com>
2025-02-11 01:51:50 -0600

    Added Sign In Test Btn / Landing Page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,6 @@
+"use client"
+
+import { useState } from 'react'
 import DailyActivities from "./components/DailyActivities";
 import MoreTraining from "./components/MoreTraining";
 import HeroBanner from "./components/HeroBanner";
@@ -6,11 +9,18 @@ import LeaderboardLanding from "./components/LeaderboardLanding";
 import Features from './components/Features';
 import HowItWorks from './components/HowItWorks';
 import Testimonials from './components/Testimonials';
+import { Button } from '@/components/ui/button';
 export default function Home() {
-  const isSignedIn = true;
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen">
       <HeroBanner />
+      <Button
+        className="text-white bg-teal-700"
+        onClick={() => setIsSignedIn(!isSignedIn)}>
+        {isSignedIn ? 'Signed In' : 'Sign In'}
+      </Button>
       {isSignedIn ?
         //Signed In Landing Page
         <>
