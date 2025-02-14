b7413f7608a633b4f6413a642b181c92b8916aad
Coding With Thang <codingwiththang@gmail.com>
2025-02-13 21:51:59 -0600

    Added mongoDB connection and save users from Clerk

--- src/app/api/webhooks/routes.js
+++ src/app/api/webhooks/routes.js
@@ -54,12 +54,45 @@ export async function POST(req) {
   console.log(`Received webhook with ID ${id} and event type of ${eventType}`);
   console.log("Webhook payload:", body);
 
-  if (evt.type === "user.created") {
-    console.log("userId:", evt.data.id);
+  if (eventType === "user.created" || eventType === "user.updated") {
+    const { id, first_name, last_name, username } = evt?.data;
+    try {
+      const user = await createOrUpdateUser(
+        id,
+        first_name,
+        last_name,
+        username
+      );
+      if (user && eventType === "user.created") {
+        try {
+          await clerkClient.users.updateUserMetadata(id, {
+            publicMetadata: {
+              userMongoId: user._id,
+              isAdmin: user.isAdmin,
+            },
+          });
+        } catch (error) {
+          console.log("Error updating user metadata:", error);
+        }
+      }
+    } catch (error) {
+      console.log("Error creating or updating user:", error);
+      return new Response("Error occurred", {
+        status: 400,
+      });
+    }
   }
 
-  if (eve.type === "use.updated") {
-    console.log("user is updated:", evt.data.id);
+  if (eventType === "user.deleted") {
+    const { id } = evt?.data;
+    try {
+      await deleteUser(id);
+    } catch (error) {
+      console.log("Error deleting user:", error);
+      return new Response("Error occurred", {
+        status: 400,
+      });
+    }
   }
 
   return new Response("Webhook received", { status: 200 });
