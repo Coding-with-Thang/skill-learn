fd0cdcac683ce358e63126c5091937384fcd2f15
Coding With Thang <codingwiththang@gmail.com>
2025-02-13 22:15:01 -0600

    Added sign-up page test

---
+++ src/app/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { SignUp } from '@clerk/nextjs'
+
+export default function SignUpPage() {
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      {/* Hero Banner */}
+      <div className="w-1/2 bg-cover bg-center" style={{ backgroundImage: "url('/hero-image.jpg')" }} />
+
+      {/* Sign In Form */}
+      <div className="w-1/2 flex justify-center items-center px-8 py-12 bg-white shadow-md">
+        <SigUp />
+      </div>
+    </div>
+  )
+}
