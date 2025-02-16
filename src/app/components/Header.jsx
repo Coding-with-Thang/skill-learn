ee7e77edd37dddfdeb66699fdfbf1f3e846e71c0
Coding With Thang <codingwiththang@gmail.com>
2025-02-16 12:30:53 -0600

    Refactored MainNav into Header

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -2,9 +2,12 @@
 
 import { useState } from 'react';
 import Link from "next/link";
-import MainNav from "./MainNav";
+import Image from "next/image"
 import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
-import { Star } from 'lucide-react';
+import { Star, Telescope, Medal, BookOpenText, ChartBar, Search } from 'lucide-react';
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import logo from '../../../public/logo.svg'
 
 export default function Header() {
 
@@ -14,27 +17,61 @@ export default function Header() {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <header className="sticky top-0 w-full bg-white border-b-2 z-[1000]">
       <div className="flex items-center h-16 justify-between">
-        <MainNav />
+        <div className="hidden md:flex items-center pl-5">
+          <Link href="/" className="flex flex-1 gap-2 font-bold text-2xl mx-2 lg:mx-4 text-nowrap">
+            <Image
+              src={logo}
+              width={48}
+              alt="Skill-Learn"
+            />
+            Skill-Learn
+          </Link>
+          <form onSubmit={handleSubmit} className="flex border border-gray-400 shadow-md rounded-lg">
+            <Input
+              type="text"
+              placeholder="Search..."
+              className="flex items-center outline-none border-none shadow-none focus:border-none focus:outline-none"
+            //   value={searchTerm}
+            // onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <Button className="items-center outline-none border-none shadow-none">
+              <Search />
+            </Button>
+          </form>
+
+          {/* <nav className="hidden lg:flex items-center justify-center gap-3 lg:gap-4 mx-3 lg:mx-9">
+        <Link href="/about" className=" hover:underline">
+          About
+        </Link>
+        <Link href="/latest-updates" className=" hover:underline">
+          Latest Updates
+        </Link>
+      </nav> */}
+        </div>
         <div className="flex items-center justify-end gap-6 mr-6 lg:mr-9">
-          <Link href="/discover" className="hover:underline">
-            Discover
+          <Link href="/discover" className="flex gap-1 items-center hover:underline">
+            <Telescope /> Discover
           </Link>
-          <Link href="/training" className="hover:underline">
-            Training
+          <Link href="/training" className="flex gap-1 items-center hover:underline">
+            <BookOpenText /> Training
           </Link>
-          <Link href="/achivements" className="hover:underline">
-            Achievements
+          <Link href="/achievements" className="flex gap-1 items-center hover:underline">
+            <Medal /> Achievements
           </Link>
-          <Link href="/rewards" className=" flex  gap-1 items-center hover:underline">
+          <Link href="/rewards" className="flex gap-1 items-center hover:underline">
             <Star /> Rewards
           </Link>
 
           <SignedIn>
-            <Link href="/dashboard" className="hover:underline">
-              Dashboard
+            <Link href="/dashboard" className="flex gap-1 items-center hover:underline">
+              <ChartBar />Dashboard
             </Link>
 
             <div className="relative">
@@ -101,6 +138,6 @@ export default function Header() {
           </SignedOut>
         </div>
       </div>
-    </header>
+    </header >
   );
 }
