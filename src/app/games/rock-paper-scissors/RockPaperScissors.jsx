94bf4f8d9a8ed1e746112c6427c6a9d9f44f572c
Coding With Thang <codingwiththang@gmail.com>
2025-06-03 12:34:37 -0500

    Added rules and game reset function

--- src/app/games/rock-paper-scissors/RockPaperScissors.jsx
+++ src/app/games/rock-paper-scissors/RockPaperScissors.jsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from 'react';
 import useLocalStorage from "../../../lib/hooks/useLocalStorage";
 import { usePathname } from 'next/navigation';
+import { Button } from "@/components/ui/button";
 import QuizModal from "../../components/Quiz/QuizModal"
 
 const choices = ['Rock', 'Paper', 'Scissors'];
@@ -45,6 +46,14 @@ export default function RockPaperScissors() {
     }
   };
 
+  //
+  const resetGame = () => {
+    setPlayerChoice(null);
+    setComputerChoice(null);
+    setResult('');
+    setRound((prev) => (prev >= 3 ? 3 : prev + 1));
+  }
+
   return (
     <div className="text-center">
       <h2 className="text-2xl font-bold mb-4">Rock-Paper-Scissors</h2>
@@ -55,13 +64,14 @@ export default function RockPaperScissors() {
       </div>
       <div className="space-x-4">
         {choices.map((choice) => (
-          <button
+          <Button
             key={choice}
             onClick={() => playGame(choice)}
             className="px-4 py-2 bg-blue-500 text-white rounded"
+            disabled={playerChoice !== null} // Disable buttons after a choice is made
           >
             {choice}
-          </button>
+          </Button>
         ))}
       </div>
       {playerChoice && (
@@ -72,11 +82,23 @@ export default function RockPaperScissors() {
         </div>
       )}
 
+      <div className="my-6 flex justify-center items-center">
+        <Button
+          onClick={() => resetGame()}
+          className="px-4 py-2 bg-gray-900 text-white rounded"
+          disabled={playerChoice !== null} // Disable buttons after a choice is made
+        >
+          New Game
+        </Button>
+      </div>
+
       <div className='mt-20 border rounded-xl py-4 px-8'>
         <h3 className='text-lg font-semibold'>Rules: </h3>
         <div>
-          <p>1. Guess a number from 1-100</p>
-          <p>2. Guess the right number and WIN!</p>
+          <p>1. Pick one: ROCK, PAPER, SCISSORS</p>
+          <p>2. Oppenent will pick one as well</p>
+          <p>3. If you have the same choice as your opponent, it'll be a tie.</p>
+          <p>4. Otherwise, rock beats scissors. Scissors beats paper. Paper beats rock.</p>
         </div>
       </div>
 
