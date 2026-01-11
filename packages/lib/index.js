1d0847d8020d8fbb357098d66a9a5cc013ce8cca
Coding With Thang <codingwiththang@gmail.com>
2026-01-11 00:39:26 -0600

    Refactor export paths in index.js and update package.json to align with new directory structure for utilities, hooks, and stores. This enhances organization and consistency across the codebase.

--- packages/lib/index.js
+++ packages/lib/index.js
@@ -1,26 +1,26 @@
 // Export utilities
-export { cn } from './utils.js';
-export * from './zodSchemas.js';
+export { cn } from "./utils.js";
+export * from "./zodSchemas.js";
 
 // Export commonly used utils
-export * from './utils/axios.js';
-export * from './utils/errorHandler.js';
-export * from './utils/apiWrapper.js';
+export * from "./utils/utils/axios.js";
+export * from "./utils/utils/errorHandler.js";
+export * from "./utils/utils/apiWrapper.js";
 
 // Export hooks (re-export from hooks directory)
-export { useAppTheme } from './hooks/useAppTheme.js';
-export { useDebounce } from './hooks/useDebounce.js';
-export { useLocalStorage } from './hooks/useLocalStorage.js';
-export { useMobile } from './hooks/useMobile.js';
-export { usePageTracking } from './hooks/usePageTracking.js';
-export { useUserRole } from './hooks/useUserRole.js';
+export { useAppTheme } from "./hooks/hooks/useAppTheme.js";
+export { useDebounce } from "./hooks/hooks/useDebounce.js";
+export { useLocalStorage } from "./hooks/hooks/useLocalStorage.js";
+export { useMobile } from "./hooks/hooks/useMobile.js";
+export { usePageTracking } from "./hooks/hooks/usePageTracking.js";
+export { useUserRole } from "./hooks/hooks/useUserRole.js";
 
 // Export stores (re-export from stores directory)
-export * from './stores/auditLogStore.js';
-export * from './stores/categoryStore.js';
-export * from './stores/coursesStore.js';
-export * from './stores/pointsStore.js';
-export * from './stores/progressStore.js';
-export * from './stores/quizStore.js';
-export * from './stores/rewardStore.js';
-export * from './stores/usersStore.js';
+export * from "./stores/store/auditLogStore.js";
+export * from "./stores/store/categoryStore.js";
+export * from "./stores/store/coursesStore.js";
+export * from "./stores/store/pointsStore.js";
+export * from "./stores/store/progressStore.js";
+export * from "./stores/store/quizStore.js";
+export * from "./stores/store/rewardStore.js";
+export * from "./stores/store/usersStore.js";
